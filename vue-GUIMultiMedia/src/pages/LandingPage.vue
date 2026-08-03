a416bb95c069e405f66fd3c51aa4803208236ebc
Aqbar-firmansyah <akbar.frmnsyhh@gmail.com>
2026-08-03 11:23:51 +0700

    push

---
+++ vue-GUIMultiMedia/src/pages/LandingPage.vue
@@ -0,0 +1,6 @@
+<template>
+ <main>
+ <h1>Selamat Datang di Onlineshop Sekolah</h1>
+ <p>Belanja gampang, sekolah senang.</p>
+ </main>
+</template>
---
+++ vue-GUIMultiMedia/src/pages/ProductPage.vue
@@ -0,0 +1,6 @@
+<template>
+ <main>
+ <h1>Produk Kami</h1>
+ <p>(kartu produk akan kita tambahkan di Tahap 8)</p>
+ </main>
+</template>
